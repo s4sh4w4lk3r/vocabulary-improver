563068bc3a16a2aefed7dd2e1f8541778f81391a
BerlogaPCWIN <sanya.romaniuta@gmail.com>
2023-04-22 17:39:48 +0300

    добавил добавление из тхт в жсон

--- console/FileProcessing.cs
+++ console/FileProcessing.cs
@@ -4,8 +4,8 @@ namespace console;
 class FileProcessing
 {
     #region internal
-    public string Path {get; private set;} // Путь до JSON файла.
-    public List<Word>? Dict {get; private set;} // Список объектов Word.
+    public string Path { get; private set; } // Путь до JSON файла.
+    public List<Word>? Dict { get; private set; } // Список объектов Word.
     public List<Word>? ShuffledList // Свойство, перемешивающее список объектов Word для вывода.
     {
         get { return Dict?.OrderBy(x => new Random().Next()).ToList(); }
@@ -23,12 +23,12 @@ class FileProcessing
     public void AddFromTxt(string path) // Чтение из тектосого файла в текущий список объектов Word, если файл не имеет ниодной строки, то ничего не произойдет.
     {
         Dict = new List<Word>();
-        
+
 
         List<string> lines = System.IO.File.ReadLines(path).ToList();
         if (lines.Count == 0) return;
 
-        try 
+        try
         {
             foreach (var item in lines)
             {
@@ -37,7 +37,7 @@ class FileProcessing
             }
             SaveToJSON();
         }
-        catch (System.IndexOutOfRangeException) 
+        catch (System.IndexOutOfRangeException)
         {
             throw new Exception($"Incorrect file formatting.\n{path}");
         }
@@ -76,7 +76,7 @@ class FileProcessing
     public void Remove(string key) // Удалить слово по ключу из JSON.
     {
         Word? word = Dict?.Find(x => x.Key.Contains(key));
-        Dict?.Remove(word!); 
+        Dict?.Remove(word!);
         SaveToJSON();
     }
     #endregion
@@ -86,7 +86,7 @@ class FileProcessing
         System.Console.WriteLine();
         System.Console.Write("File name:");
         string path = Console.ReadLine()!;
-        try 
+        try
         {
             CreateFile(path);
             System.Console.WriteLine("File Created");
@@ -102,16 +102,16 @@ class FileProcessing
     {
         System.Console.WriteLine("1 - Select a file in the current directory, 2 - Specify the file path");
         switch (Console.ReadKey(true).Key)
-            {
-                case ConsoleKey.D1:
-                    FileProcessing.ScanDir();
-                    ConsoleOpenFile();
-                    break;
-                case ConsoleKey.D2:
-                    FileProcessing.SpecifyDir();
-                    ConsoleOpenFile();
-                    break;
-            }
+        {
+            case ConsoleKey.D1:
+                FileProcessing.ScanDir();
+                ConsoleOpenFile();
+                break;
+            case ConsoleKey.D2:
+                FileProcessing.SpecifyDir();
+                ConsoleOpenFile();
+                break;
+        }
     }
     static void ScanDir()
     {
@@ -130,19 +130,19 @@ class FileProcessing
 
         int.TryParse(fileNumberString, out int fileNumberInt);
 
-        if (fileNumberInt <0 || fileNumberInt > files.Length -1)
+        if (fileNumberInt < 0 || fileNumberInt > files.Length - 1)
         {
             System.Console.WriteLine("\nBad file number.");
             ScanDir();
         }
         string filePath = files[fileNumberInt];
 
-        try 
+        try
         {
             FileProcessing file = new FileProcessing(filePath);
             FileSelected(file);
         }
-        
+
         catch (Exception)
         {
             System.Console.WriteLine("Incorrect file formatting.");
@@ -153,7 +153,7 @@ class FileProcessing
     {
         System.Console.Write("Enter the path: ");
         string path = Console.ReadLine()!;
-        try 
+        try
         {
             FileProcessing file = new FileProcessing(path);
             FileSelected(file);
@@ -166,14 +166,33 @@ class FileProcessing
     }
     static void FileSelected(FileProcessing file)
     {
-        if (file.ShuffledList is null) 
+        System.Console.WriteLine("1 - Start, 2 - Fill dictionary from file");
+        if (Console.ReadKey(true).Key == ConsoleKey.D1)
         {
-            System.Console.WriteLine("Dictionary is null. Exiting...");
-            Improver.ShutDown();
+            if (file.ShuffledList is null)
+            {
+                System.Console.WriteLine("Dictionary is null. Exiting...");
+                Improver.ShutDown();
+            }
+            VIDictionary vIDictionary = new VIDictionary(file, file.ShuffledList!);
+            Improver improver = new Improver(vIDictionary);
+            improver.Start();
         }
-        VIDictionary vIDictionary = new VIDictionary(file, file.ShuffledList!);
-        Improver improver = new Improver(vIDictionary);
-        improver.Start();
+        if (Console.ReadKey(true).Key == ConsoleKey.D2)
+        {
+            System.Console.Write("Enter the path: ");
+            string path = Console.ReadLine()!;
+            try 
+            {
+                file.AddFromTxt(path);
+                System.Console.WriteLine("OK");
+                FileSelected(file);
+            }
+            catch
+            {
+                System.Console.WriteLine("Bad file.");
+            }
+        }
     }
     #endregion
 }
